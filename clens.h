58e06c34abb344c433cee28d5c87b45dd8277ce5
Dave Collins <davec@conformal.com>
2011-04-18 18:23:07 +0000

    Initial import.

---
+++ clens.h
@@ -0,0 +1,32 @@
+/* $clens$ */
+/*
+ * Copyright (c) 2010 Conformal Systems LLC <info@conformal.com>
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * This file contains ifdefs and #defines to bring the OS into focus
+ * so that the source can build on any supported platform.
+ */
+
+#ifdef __FreeBSD__
+#define SA_LEN(x)      ((x)->sa_len)
+#define __dead __dead2
+
+#define NEED_ARC4RANDOM_BUF
+void arc4random_buf(void *buf, size_t nbytes);
+#define NEED_STRNVIS
+int strnvis(char *dst, const char *src, size_t siz, int flag);
+#endif /* __FreeBSD__ */
